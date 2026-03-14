41452532b3f60b94ede53af241bc653fb6da7576
soydavido <44959528+soydavido@users.noreply.github.com>
2026-03-14 09:25:01 -0400

    FEAT: Update card

--- src/api/card/card.service.ts
+++ src/api/card/card.service.ts
@@ -4,6 +4,7 @@ import { BaseService } from 'src/common/services/base.service';
 import { getEnv } from 'src/common/utils/env';
 import { CardEntity } from 'src/database/models/card.entity';
 import { DataSource } from 'typeorm';
+import { UpdateCardDto } from './dto/update-card.dto';
 
 @Injectable()
 export class CardService extends BaseService<CardEntity> {
@@ -14,4 +15,11 @@ export class CardService extends BaseService<CardEntity> {
   async delete(id: string, userSecret: string) {
     return this.repository.delete({ idCard: Number(id), userSecret });
   }
+
+  async updateCard(id: string, userSecret: string, updateCardDto: UpdateCardDto) {
+  return this.repository.update(
+    { idCard: Number(id), userSecret }, 
+    updateCardDto
+  );
+}
 }
